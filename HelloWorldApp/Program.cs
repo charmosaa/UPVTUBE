532813751437c57e3e21896c413920d14a4a644a
Karol Waliszewski <KWALISZ@upv.edu.es>
2023-09-28 15:58:38 +0200

    Added HelloWorld app with printing nationalities.

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nationalities;
+            void initList(){
+                nationalities = new List<string>(){
+                 "Australian",
+                 "Mongolian",
+                 "Russian",
+                 "Austrian",
+                 "Brazilian"
+                };
+            }
+            initList();
+            nationalities.Sort();
+            foreach (string nationality in nationalities){
+                Console.WriteLine(nationality);
+            }
+        }
+    }
+}
