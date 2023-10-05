6cc881702d1e6db0db7167f847bebbaed056a54b
Leonardo Mantovani <LMANTOV@upv.edu.es>
2023-10-05 16:33:53 +0200

    Implemented relationships and attributes of Member class

--- ClassLibrary/BusinessLogic/Entities/Member.cs
+++ ClassLibrary/BusinessLogic/Entities/Member.cs
@@ -1,8 +1,27 @@
-﻿namespace UpvTube.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace UpvTube.Entities
 {
 	public partial class Member
 	{
+		public Member()
+		{
+			Comments = new List<Comment>();
+			Contents = new List<Content>();
+			Evaluations = new List<Evaluation>();
+			SubscribedTo = new List<Member>();
+			Visualizations = new List<Visualization>();
+		}
 
+		public Member(string email, string fullName, DateTime lastAccessDate, string nick, string password) : this()
+		{
+			this.Email = email;
+			this.FullName = fullName;
+			this.LastAccessDate = lastAccessDate;
+			this.Nick = nick;
+			this.Password = password;
+		}
 	}
 }
 
--- ClassLibrary/Persistence/Entities/Member.cs
+++ ClassLibrary/Persistence/Entities/Member.cs
@@ -1,8 +1,38 @@
-﻿namespace UpvTube.Entities
+﻿using System;
+using System.Collections.Generic;
+
+namespace UpvTube.Entities
 {
 	public partial class Member
 	{
+        #region ATTRIBUTES
 
-	}
+        public string Email { get; set; }
+        
+        public string FullName { get; set; }
+        
+        public virtual DateTime LastAccessDate { get; set; }
+
+        public virtual string Nick { get; set; }
+
+        public virtual string Password { get; set; }
+
+        #endregion
+
+
+        #region RELATIONSHIPS
+
+        public virtual List<Comment> Comments { get; set; }
+
+        public virtual List<Content> Contents { get; set; }
+
+        public virtual List<Evaluation> Evaluations { get; set; }
+
+        public virtual List<Member> SubscribedTo { get; set; }
+
+        public virtual List<Visualization> Visualizations { get; set; }
+
+        #endregion
+    }
 }
 
