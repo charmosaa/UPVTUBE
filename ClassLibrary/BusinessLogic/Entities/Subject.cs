511c6f8f872ee1d9178abf63323442e8c4b4b405
karolwaliszewski <KWALISZ@upv.edu.es>
2023-10-05 16:43:48 +0200

    Created constructors for Content & Subject

--- ClassLibrary/BusinessLogic/Entities/Subject.cs
+++ ClassLibrary/BusinessLogic/Entities/Subject.cs
@@ -1,8 +1,22 @@
-﻿namespace UpvTube.Entities
+﻿using System.Collections.Generic;
+
+namespace UpvTube.Entities
 {
 	public partial class Subject
 	{
+        public Subject()
+        {
+            this.Contents = new List<Content>();
+        }
 
-	}
+        public Subject(string fullName, string name) : this()
+        {
+            this.FullName = fullName;
+            this.Name = name;
+
+            this.Contents = new List<Content>();
+        }
+
+    }
 }
 
