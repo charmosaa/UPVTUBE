c1be884eff9d865bbe449f20118decd3539fa8e4
Martyna Lopianiak <MLOPIAN@upv.edu.es>
2023-10-05 16:45:25 +0200

    implementation of comment class and relationships and atributes of comment class and evaluation class

--- ClassLibrary/Persistence/Entities/Comment.cs
+++ ClassLibrary/Persistence/Entities/Comment.cs
@@ -1,8 +1,14 @@
-﻿namespace UpvTube.Entities
-{
-	public partial class Comment
-	{
+﻿using System;
 
-	}
+namespace UpvTube.Entities
+{
+    public partial class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime WritingDate { get; set; }
+        public virtual Member Writer { get; set; }
+        public virtual Content Content { get; set; }
+    }
 }
 
