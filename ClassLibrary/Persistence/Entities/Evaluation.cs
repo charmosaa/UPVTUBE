c5415eb3a3a869d42e4b1940ef6e1c7b1a4f88d3
Arkadiusz Dawid <ADAWID@upv.edu.es>
2023-10-19 16:39:49 +0200

    Added data annotations and missed Id property

--- ClassLibrary/Persistence/Entities/Evaluation.cs
+++ ClassLibrary/Persistence/Entities/Evaluation.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace UPVTube.Entities
 {
-	public partial class Evaluation
-	{
-		public int Id { get; set; }
-		public string RejectionReason { get; set; }
-		public DateTime EvaluationDate { get; set; }
-		public virtual Member Censor { get; set; }
-		public virtual Content Content { get; set; }
-	
-	}
+    public partial class Evaluation
+    {
+        public int Id { get; set; }
+
+        public string RejectionReason { get; set; }
+
+        public DateTime EvaluationDate { get; set; }
+
+        [Required]
+        public virtual Member Censor { get; set; }
+
+        [Required]
+        public virtual Content Content { get; set; }
+    }
 }
 
