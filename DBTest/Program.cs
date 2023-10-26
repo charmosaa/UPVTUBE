22046eb3a27f510cd19cc7db92390548198f7f73
Karol Waliszewski <KWALISZ@upv.edu.es>
2023-10-26 16:24:25 +0200

    Added the rest of entities to Program.cs

--- DBTest/Program.cs
+++ DBTest/Program.cs
@@ -80,7 +80,6 @@ namespace DBTest
             dal.Commit();
 
             Console.WriteLine("\n// CREATING A CONTENT");
-
             //public Content(string contentURI, string description, bool isPublic, string title, DateTime uploadDate, Member owner)
 
             Content c1 = new Content("http://fake.uri.es", "Inheritance driven polymorphism explained", true, "Polymorphism", DateTime.Now, a1);
@@ -88,23 +87,50 @@ namespace DBTest
             dal.Insert<Content>(c1);
             dal.Commit();
 
-            Console.WriteLine("A new video has been uploaded to: " + c1.ContentURI + " created by " + c1.Owner.FullName);
-
-
             Console.WriteLine("\n// CREATING A MEMBER - PROFESSOR");
             // public Member(String email, string fullName, DateTime lastAccessDate, string nick, string password)
 
+            Member p1 = new Member("john@inf.upv.es", "John Doe", DateTime.Now, "Prof", "4321");
+            dal.Insert<Member>(a1);
+            dal.Commit();
+
             Console.WriteLine("\n// CREATING A EVALUATION");
-            // public Evaluation()
+            // public Evaluation(DateTime evaluationDate, string rejectionReason, Member censor, Content content)
+
+            Evaluation e1 = new Evaluation(DateTime.Now, null, p1, c1);
+            c1.Evaluation = e1;
+            p1.Evaluations.Add(e1);
+            dal.Insert<Evaluation>(e1);
+            dal.Commit();
 
             Console.WriteLine("\n// CREATING A VISUALIZATION");
-            // public Visualization()
+            // public Visualization(DateTime dateTime, Content content, Member member)
+
+            Visualization v1 = new Visualization(DateTime.Now, c1, p1);
+            c1.Visualizations.Add(v1);
+            p1.Visualizations.Add(v1);
+            dal.Insert<Visualization>(v1);
+            dal.Commit();
 
             Console.WriteLine("\n// CREATING A COMMENT");
-            // public Comment()
+            // public Comment(string Text, DateTime WritingDate, Content Content, Member Writer)
 
-            // Populate here the rest of the database with data
+            Comment cm1 = new Comment("This is awesome!", DateTime.Now, c1, a1);
+            c1.Comments.Add(cm1);
+            a1.Comments.Add(cm1);
+            dal.Insert<Comment>(cm1);
+            dal.Commit();
 
+            Console.WriteLine("\n// CREATING A SUBJECT");
+            // public Subject(int code, string fullName, string name)
+
+            Subject s1 = new Subject(1, "Software Engineering", "ISW");
+            s1.Contents.Add(c1);
+            c1.Subjects.Add(s1);
+            dal.Insert<Subject>(s1);
+            dal.Commit();
+
+            Console.WriteLine("A new video has been uploaded to: " + c1.ContentURI + " created by " + c1.Owner.FullName);
 
             Console.ReadKey();
         }
