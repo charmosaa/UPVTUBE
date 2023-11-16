427deef3b5e04757654e08178c3b63454cf7b39e
Karol Waliszewski <KWALISZ@upv.edu.es>
2023-11-16 16:01:40 +0100

    Added logged user field to UpvTubeService

---
+++ ClassLibrary/BusinessLogic/Services/UPVTubeService.cs
@@ -0,0 +1,52 @@
+﻿using UPVTube.Entities;
+using UPVTube.Persistence;
+
+namespace UpvTube.BusinessLogic.Services
+{
+    public class UPVTubeService : IUPVTubeService
+    {
+
+        private readonly IDAL dal;
+        private Member logged;
+
+        public UPVTubeService(IDAL dal) {
+            this.dal = dal;
+            this.logged = null;
+        }
+
+        public void addReviewToPendingContent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void displayContentDetails()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void login()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void logout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void registerNewUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void searchContent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void uploadNewContent()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
