acac320b3fcbca8a626e21098d4c020fab2abba6
Karol Waliszewski <KWALISZ@upv.edu.es>
2023-11-23 16:59:43 +0100

    Added Arek's changes

--- ClassLibrary/BusinessLogic/Services/IUPVTubeService.cs
+++ ClassLibrary/BusinessLogic/Services/IUPVTubeService.cs
@@ -7,7 +7,7 @@ namespace UpvTube.BusinessLogic.Services
         void registerNewUser(string email, string fullname, string nick, string password);
         void login(string nick, string password);
         void logout();
-        void uploadNewContent();
+        void uploadNewContent(string contentURI, string description, bool isPublic, string title, ICollection<Subject> subjects);
         void searchContent();
         Content displayContentDetails(int id);
         void addReviewToPendingContent();
