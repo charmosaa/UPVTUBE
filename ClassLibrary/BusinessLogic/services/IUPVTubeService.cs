1a6283affc6f94aa4b3728bb34a35c060c61a62c
Karol Waliszewski <KWALISZ@upv.edu.es>
2023-11-23 15:52:43 +0100

    Added fullname argument to registerNewUser method in the interface

--- ClassLibrary/BusinessLogic/services/IUPVTubeService.cs
+++ ClassLibrary/BusinessLogic/services/IUPVTubeService.cs
@@ -2,7 +2,7 @@ namespace UpvTube.BusinessLogic.Services
 {
     interface IUPVTubeService
     {
-        void registerNewUser(string email, string nick, string password);
+        void registerNewUser(string email, string fullname, string nick, string password);
         void login(string nick, string password);
         void logout();
         void uploadNewContent();
