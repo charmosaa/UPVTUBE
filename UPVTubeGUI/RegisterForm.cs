01cc118501d761ae2a06aedc3985ab31f471b888
Martyna Lopianiak <MLOPIAN@upv.edu.es>
2023-12-14 16:53:23 +0100

    created register form

---
+++ UPVTubeGUI/RegisterForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UpvTube.BusinessLogic.Services;
+using UPVTube.Services;
+
+namespace UPVTubeGUI
+{
+    public partial class RegisterForm : UPVTubeBase
+    {
+        public RegisterForm(IUPVTubeService service) : base(service)
+        {
+            InitializeComponent();
+            service.DBInitialization();
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                service.RegisterNewUser(emailTextBox.Text,fullNameTextBox.Text,nickTextBox.Text, passwordTextBox.Text);
+                DialogResult answer = MessageBox.Show(this, "user registered", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (ServiceException ex)
+            {
+                DialogResult answer = MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
